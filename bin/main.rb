c9ba8abec0a647e0323b726307a4ed293ae46d71
Michael McGee <michael@mcgee.io>
2019-07-31 17:15:20 +0700

    Start main.rb game loop

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,32 @@
 # main.rb
+require '../lib/player'
+
+loop do
+  #   create_game
+  #   create_board
+  puts "Welcome to Tic Tac Toe!"
+
+  print "Please enter name of player 1: "
+  player_1_name = gets.chomp
+  # player_1 = Player.new(player_1_name, "X")
+  puts "Player 1 is: #{player_1.name}"
+
+
+  print "Please enter name of player 2: "
+  player_2_name = gets.chomp
+  # player_2 = Player.new(player_2_name, "O")
+  puts "Player 2 is: #{player_2.name}"
+
+
+  #   loop do
+  #     game.turn(current_player)
+  #     break if game.winner? || game.draw?
+  #   end
+
+  #   game.display_winner || game.display_draw
+
+  print "Would you like to play again? (y/n): "
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+
+end
