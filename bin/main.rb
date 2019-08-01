e8eec2bf74cc1cbe8e6ac78bca5083249af6335d
pelzolga123 <pelzolga123@gmail.com>
2019-08-01 17:54:07 +0300

    Minor changing

--- bin/main.rb
+++ bin/main.rb
@@ -8,6 +8,7 @@ require_relative '../lib/player'
 loop do
   system 'clear'
   puts 'Welcome to Tic-Tac-Toe!'
+
   board = Board.new
   player_names = []
   2.times do |i|
@@ -17,19 +18,23 @@ loop do
   player1 = Player.new(player_names[0], 'X')
   player2 = Player.new(player_names[1], 'O')
   game = Game.new(board, player1, player2)
+
   # Player turn loop
   loop do
     game.switch_player
     system 'clear'
     puts board.display
+
     selection = ''
     loop do
       puts "#{game.current_player.name}(#{game.current_player.marker}), it's your turn."
       print 'Enter your selection (1-9): '
       selection = gets.chomp.to_i
       break if board.valid_selection?(selection) && (1..9).include?(selection)
+
       puts 'Sorry, that selection is invalid!'
     end
+
     game.turn(selection)
     break unless game.state == :active
   end
